840dd67631edd54ddc5895924e894c6e45feb5b6
Sebastiaan <sheijmann@xs4all.nl>
2014-10-13 13:13:04 +0200

    implement userstory4

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,79 +1,87 @@
 function AppCtrl ($scope) {
-    'use strict';
-    $scope.title = 'The Movie Database';
+	'use strict';
+	$scope.title = 'The Movie Database';
+}
+
+function HeaderController($scope, $location)
+{
+	'use strict';
+	$scope.isActive = function (viewLocation) {
+		return viewLocation === $location.path();
+	};
 }
 
 function WelcomeCtrl ($scope, moviesResponse) {
-    'use strict';
-    $scope.movies = moviesResponse.data;
+	'use strict';
+	$scope.movies = moviesResponse.data;
 }
 
 function MoviesListCtrl ($scope, $location, moviesResponse) {
-    'use strict';
-    $scope.movies = moviesResponse.data;
-    $scope.add = function () {
-        $location.path('/movies/new');
-    };
+	'use strict';
+	$scope.movies = moviesResponse.data;
+	$scope.add = function () {
+		$location.path('/movies/new');
+	};
 }
 
 MoviesListCtrl.resolve = {
-    moviesResponse: function ($http) {
-        'use strict';
-        return $http.get('/movies');
-    }
+	moviesResponse: function ($http) {
+		'use strict';
+		return $http.get('/movies');
+	}
 };
 
 function MoviesAddCtrl ($scope, $http, $location) {
-    'use strict';
-    $scope.movie = {};
-    $scope.save = function (movie) {
-        $http.post('/movies', movie)
-        .success(function(res) {
-            $location.path('/movies/' + res.id);
-        });
-    };
+	'use strict';
+	$scope.movie = {};
+	$scope.save = function (movie) {
+		$http.post('/movies', movie)
+			.success(function(res) {
+				$location.path('/movies/' + res.id);
+			});
+	};
 }
 
 function MovieDetailCtrl ($scope, $http, $location, moviesResponse) {
-    'use strict';
-    $scope.movie = moviesResponse.data;
+	'use strict';
+	$scope.movie = moviesResponse.data;
 
-    $scope['delete'] = function () {
-        $http['delete']('/movies/' + $scope.movie.id).success(function (res) {
-            $location.path('/movies');
-        });
-    };
+	$scope['delete'] = function () {
+		$http['delete']('/movies/' + $scope.movie.id).success(function (res) {
+			$location.path('/movies');
+		});
+	};
 }
 
 function movieDetailResolver ($http, $route) {
-    'use strict';
-    var id = $route.current.params.id;
-    return $http.get('/movies/' + id);
+	'use strict';
+	var id = $route.current.params.id;
+	return $http.get('/movies/' + id);
 }
 
 MovieDetailCtrl.resolve = {
-    moviesResponse: movieDetailResolver
+	moviesResponse: movieDetailResolver
 };
 
 function MovieEditCtrl ($scope, $http, $location, moviesResponse) {
-    'use strict';
-    $scope.movie = moviesResponse.data;
+	'use strict';
+	$scope.movie = moviesResponse.data;
 
-    $scope.save = function () {
-        $http.put('/movies/' + $scope.movie.id, $scope.movie)
-        .success(function (res) {
-            $location.path('/movies/' + $scope.movie.id);
-        });
-    };
+	$scope.save = function () {
+		$http.put('/movies/' + $scope.movie.id, $scope.movie)
+			.success(function (res) {
+				$location.path('/movies/' + $scope.movie.id);
+			});
+	};
 }
 
 MovieEditCtrl.resolve = {
-    moviesResponse: movieDetailResolver
+	moviesResponse: movieDetailResolver
 };
 
 function NotFoundCtrl ($scope, $location) {
-    'use strict';
-    $scope.culprit = $location.search().culprit || 'unknown beast';
+	'use strict';
+	$scope.culprit = $location.search().culprit || 'unknown beast';
 }
 
 var ErrorCtrl = NotFoundCtrl;
